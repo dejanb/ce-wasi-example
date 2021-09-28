2815b019c554706e1d555b745d22a58b1934d3a8
Dejan Bosanac <dejan@sensatic.net>
2021-09-28 17:37:04 +0200

    Send cloud event and prepare to parse response

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
-use cloudevents::{message::BinaryDeserializer, EventBuilder, EventBuilderV10};
+use cloudevents::{message::*, Event, EventBuilder, EventBuilderV10, binding::http::*};
 use bytes::Bytes;
 use http;
 use http::Request;
-use wasi_experimental_http;
+use wasi_experimental_http::Response;
 
 fn main() {
     const POSTMAN_ECHO_POST_URL: &str = "https://postman-echo.com/post";
+    //const POSTMAN_ECHO_POST_URL: &str = "http://echoserver.echoserver";
 
     loop {
         let builder = http::request::Builder::new()
             .uri(POSTMAN_ECHO_POST_URL)
             .method(http::Method::POST);
 
-        let event = EventBuilderV10::new()
+        let input = EventBuilderV10::new()
             .id("0001".to_string())
             .source("http://localhost/".to_string())
             .ty("test_event.test_application".to_string())
@@ -20,17 +21,31 @@ fn main() {
             .unwrap();
 
         let request: Request<Option<Bytes>> =
-            BinaryDeserializer::deserialize_binary(event, builder).unwrap();
+            BinaryDeserializer::deserialize_binary(input, builder).unwrap();
 
-        println!("{:?}", request);
+        println!("Request: {:?}", request);
 
         let mut response = wasi_experimental_http::request(request).expect("cannot make request");
         let response_body = response.body_read_all().unwrap();
         let response_text = std::str::from_utf8(&response_body).unwrap().to_string();
 
         println!("{}", response.status_code);
-        println!("{}", response_text);
+        println!("Response: {:?}", response_text);
+
+        //let output = response_to_event(response);
+        //println!("Response: {:?}", output);
 
         std::thread::sleep(std::time::Duration::new(15, 0));
     }
+}
+
+
+/// Method to transform an incoming [`Response`] to [`Event`].
+pub fn response_to_event(mut res: Response) -> Result<Event> {
+    let headers = res.headers_get_all().unwrap();
+    println!("headers {:?}", headers);
+
+    let body = res.body_read_all().unwrap();
+
+    to_event(&headers, body)
 }
