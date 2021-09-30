e15b1b2fe0a8108548213eddfed49710058de5d4
Dejan Bosanac <dejan@sensatic.net>
2021-09-30 13:19:27 +0200

    Add support for parsing response as event

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use cloudevents::{message::*, Event, EventBuilder, EventBuilderV10, binding::http::*};
+use cloudevents::{message::*, EventBuilder, EventBuilderV10, binding::http::*};
 use bytes::Bytes;
 use http;
 use http::Request;
-use wasi_experimental_http::Response;
 use std::env;
 
 fn main() {
@@ -21,6 +20,8 @@ fn main() {
             .build()
             .unwrap();
 
+        log::info!("Event: {}", input);
+
         let request: Request<Option<Bytes>> =
             BinaryDeserializer::deserialize_binary(input, builder).unwrap();
 
@@ -29,23 +30,18 @@ fn main() {
         let mut response = wasi_experimental_http::request(request).expect("cannot make request");
         let response_body = response.body_read_all().unwrap();
         let response_text = std::str::from_utf8(&response_body).unwrap().to_string();
+        let headers = response.headers_get_all().unwrap();
 
         log::info!("{}", response.status_code);
-        log::info!("Response: {:?}", response_text);
+        log::info!("Response: {:?} {:?}", headers, response_text);
 
-        // If the echo server returns event in response, this can be used to parse it
-        //let output = response_to_event(response);
-        //log::info!("Response: {:?}", output);
+        match to_event(&headers, response_body) {
+            Ok(event) => {
+                log::info!("Event {}", event)
+            }
+            Err(_) => {}
+        }
 
         std::thread::sleep(std::time::Duration::new(15, 0));
     }
-}
-
-
-/// Method to transform an incoming [`Response`] to [`Event`].
-pub fn response_to_event(mut res: Response) -> Result<Event> {
-    let headers = res.headers_get_all().unwrap();
-    let body = res.body_read_all().unwrap();
-
-    to_event(&headers, body)
 }
